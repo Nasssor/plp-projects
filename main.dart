723cc6e764f97e2406da7f333ef7ea0be47679a7
Nassor <104492499+Nasssor@users.noreply.github.com>
2023-03-29 11:31:25 +0300

    Data Types in Dart Technical Assignment

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main(){  
+    var heart_symbol = '\u2665';  
+    var laugh_symbol = '\u{1f600}';
+    int num = 7;
+    String name = "peter";
+    List names = ["mike", "john", "simon",10];
+    var student = {'Name':'Adam Doe', 'Email':'doe@gmail.com', 'RegNumber':'B11/6732/2020'};
+    print(student.values);
+    print(num);
+    print(names);
+    print(name);
+    print(heart_symbol);  
+    print(laugh_symbol);  
+} 
